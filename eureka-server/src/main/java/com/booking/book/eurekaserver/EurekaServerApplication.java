59beba5827a6eecd6c7dc598f8e93603e901a3f9
FanTamas1234 <stalker-eugene20020905@mail.ru>
2025-03-23 00:19:21 +0300

    Добавил конфигурации для всех сервисов

---
+++ eureka-server/src/main/java/com/booking/book/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.booking.book.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
---
+++ gateway-server/src/test/java/com/booking/book/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.book.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
